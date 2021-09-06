5becb94fc1fb1d6e3c676c3e671dcdb1a2d9825d
Mehedi Hasan Tarek <mhtarek@live.com>
2021-09-07 05:20:23 +0600

    Create Program 3.7.c

---
+++ Part 1/Program 3.7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n = 10;
+
+    if(n < 30){
+        printf("n is less than 30. \n");
+    }
+    if (n < 50){
+        printf("n is less than 50. \n");
+    }
+    return 0;
+}
+// As here both are If conditional, it will print both.
