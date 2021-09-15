12f275ddc9ce9acf34a0f663a61a31301dd55eae
Mehedi Hasan Tarek <mhtarek@live.com>
2021-09-15 17:12:13 +0600

    Update Program 4.10.c

--- Part 1/Program 4.10.c
+++ Part 1/Program 4.10.c
@@ -8,7 +8,7 @@ int main()
     for (n = 1; n <= 20; n++){
             printf("\n");
         for(i = 1; i <= 10; i++){
-            printf("%d X %d %c %d\n", n, i,c, n*i);
+            printf("%d X %d %c %d\n", n, i, c, n*i);
         }
     }
     return 0;
