d06479575840288f6f65ed4848a8bea8a036f8f9
Mehedi Hasan Tarek <mhtarek@live.com>
2021-10-04 20:41:48 +0600

    Create Program 6.4.c

---
+++ Part 1/Program 6.4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i, ara[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    for(i = 0; i < 10; i++){
+        printf("%d th element is : %d\n", i+1, ara[i]);
+    }
+    return 0;
+}
